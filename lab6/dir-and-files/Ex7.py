a3abcbab7f605b8d4c55bb36f3f280d2ab1ef785
Erbolsyn259 <y_omirzak@kbtu.kz>
2025-02-25 14:02:47 +0300

    lab6
    lab6

---
+++ lab6/dir-and-files/Ex7.py
@@ -0,0 +1,13 @@
+import os
+
+path = input("Enter your path for read ")
+
+content = open(path,'r')
+
+plagiat = open('plagiat.txt','w')
+
+plagiat.write(content.read())
+
+plagiat.close()
+
+#os.remove('plagiat.txt')
